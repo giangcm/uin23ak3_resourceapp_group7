f1ab98a54f6766ca95b57b1b54d35db3299ed081
muontian <89585394+muontian@users.noreply.github.com>
2023-02-20 10:47:54 +0100

    test HTML/css

--- uin23ak3_resourceapp_group7/src/App.js
+++ uin23ak3_resourceapp_group7/src/App.js
@@ -4,7 +4,28 @@ import './css/main.css'
 
 function App() {
   return (
-    <h1>Ressursarkiv</h1>
+    <div class="container">
+        <header id="header">
+            <a href="index.html" id="title">
+                <h1>RESSURSARKIV</h1>
+            </a>
+        </header>
+        <nav>
+            <a class="navBox" id="CSS" onClick="renderCategory(event.target.id)">CSS</a>
+            <a class="navBox" className="navObject" id="HTML" onClick="renderCategory(event.target.id)">HTML</a>
+            <a class="navBox" className="navObject" id="JavaScript" onClick="renderCategory(event.target.id)">javascript</a>
+            <a class="navBox" className="navObject" id="React" onClick="renderCategory(event.target.id)">React</a>
+            <a class="navBox" id="Sanity and headless CMS" onClick="renderCategory(event.target.id)">Sanity and Headless CMS</a>
+        </nav>
+        <main>
+            <section>
+                <article id="Info">
+
+                </article>
+            </section>
+        </main>
+    </div>
+
   );
 }
 
